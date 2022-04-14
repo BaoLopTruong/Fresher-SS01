0f80400d3cc5e8bbf0951f96002d81c5d7e233cb
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-14 07:44:41 +0700

    baitap1

---
+++ Baitap1/main.js
@@ -0,0 +1,6 @@
+let number = 10000.56;
+
+console.log(number.toLocaleString());
+
+var number2 = 1000 // floating point example
+console.log(number2.toLocaleString(undefined, {maximumFractionDigits:4})); // "1,234.57"
