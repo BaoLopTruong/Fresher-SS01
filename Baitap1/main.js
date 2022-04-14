a7874a72dd3a800e859277b2276569a6bfb4aea9
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-14 08:04:54 +0700

    baitap1

--- Baitap1/main.js
+++ Baitap1/main.js
@@ -4,3 +4,8 @@ console.log(number.toLocaleString());
 
 var number2 = 1000 // floating point example
 console.log(number2.toLocaleString(undefined, {maximumFractionDigits:4})); // "1,234.57"
+
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
